7ca8c70e89f91c6aeb0a1d8bcc786bd0ef977f0c
colne5u <colne5u@etu.univ-lorraine.fr>
2020-01-15 17:24:20 +0100

    test Ecrire

--- src/tests/EcrireTest.java
+++ src/tests/EcrireTest.java
@@ -1,21 +1,38 @@
 package tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import yal.arbre.expressions.ConstanteEntiere;
+import yal.arbre.instructions.Ecrire;
 
 class EcrireTest {
 
+    Ecrire ecrire;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void verifier() {
+        ecrire = new Ecrire(new ConstanteEntiere("5", 17), 17);
     }
 
     @Test
     void toMIPS() {
+        String testEcrire;
+
+        testEcrire = "\t#ecrire 5\n" +
+                "\tli $v0, 5\t#toMips() de ConstanteEntière\n" +
+                "\tmove $a0, $v0 # $a0 = $v0\n" +
+                "\tli $v0, 1\n" +
+                "\tsyscall\n";
+
+        Assertions.assertEquals(testEcrire, ecrire.toMIPS());
+
+        testEcrire = "\t#ecrire 5\n" +
+                "\tli $v0, 5\t#toMips() de ConstanteEntière\n" +
+                "\tmove $a0, $v0 # $a0 = $v0\n" +
+                "\tli $v0, 1\n" +
+                "\tsyscall";
+
+        Assertions.assertNotEquals(testEcrire, ecrire.toMIPS());
     }
 }
