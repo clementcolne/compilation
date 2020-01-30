403606adbb477db95a1da0bcf0422729fa0877c4
MalineV <43404047+MalineV@users.noreply.github.com>
2020-01-30 12:45:02 +0100

    Add files via upload

---
+++ Yal1/yal/arbre/expressions/Idf.java
@@ -0,0 +1,55 @@
+package yal.arbre.expressions;
+
+import yal.arbre.declaration.Tds;
+import yal.exceptions.AnalyseSemantiqueException;
+
+public class Idf extends Expression{
+
+    //private int noLig;
+    private String nom;
+
+    /**
+     * Constructeur de l'Idf
+     * @param nom String
+     * @param n int
+     */
+    public Idf(String nom, int n){
+        super(n);
+        this.nom = nom;
+        this.noLigne = n;
+    }
+
+    /**
+     * Vérifie si la variable est déjà déclarée ou pas
+     * @throws AnalyseSemantiqueException
+     */
+    @Override
+    public void verifier() throws AnalyseSemantiqueException{
+        try {
+            Tds.getInstance().identifier(nom);
+        }catch (Exception e){
+            Tds.getInstance().ajoutErreur();
+            AnalyseSemantiqueException a = new AnalyseSemantiqueException(noLigne, ": variable non déclarée");
+            Tds.getInstance().add(a.getMessage());
+        }
+    }
+
+    /**
+     * Renvoie le code Mips de la variable
+     * @return String
+     */
+    @Override
+    public String toMIPS() {
+        return null;
+    }
+
+    /**
+     * Renvoie le nom de la variable
+     * @return String
+     */
+    @Override
+    public String getNom() {
+        return nom;
+    }
+
+}
---
+++ Yal1/yal/exceptions/SemantiqueException.java
@@ -0,0 +1,8 @@
+package yal.exceptions;
+
+public class SemantiqueException extends AnalyseException {
+
+    public SemantiqueException(String m) {
+        super(m);
+    }
+}
