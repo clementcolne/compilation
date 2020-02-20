a3efc5cec83dcb587908ae5832c783d505400bc2
colne5u <clement.colne@outlook.com>
2020-02-20 14:53:17 +0100

    .

--- src/yal/arbre/declaration/Fonction.java
+++ src/yal/arbre/declaration/Fonction.java
@@ -4,11 +4,6 @@ import yal.arbre.ArbreAbstrait;
 
 public class Fonction extends ArbreAbstrait{
 
-    /* |
-                FONCTION IDF:i DEBUT LINST:li FIN
-                {: Tds.getInstance().ajouter(new Entree(i), new Symbole("fonction", ileft + 1,Tds.getInstance().getBlocCourant())) ;
-                RESULT = new Fonction(li, ileft + 1) ; :}*/
-
     public Fonction(ArbreAbstrait a, int n){
         super(n);
     }
