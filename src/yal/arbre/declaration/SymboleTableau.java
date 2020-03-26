56033132bd066e913c380ed119b417bece9fe0d8
colne5u <clement.colne@outlook.com>
2020-03-26 17:40:37 +0100

    ajout message erreur taille tab 0 ou out of bound + toMIPS symbole fonction

--- src/yal/arbre/declaration/SymboleTableau.java
+++ src/yal/arbre/declaration/SymboleTableau.java
@@ -22,4 +22,17 @@ public class SymboleTableau extends Symbole {
     public boolean isTableau() {
         return true;
     }
+
+    public String toMIPS() {
+        StringBuilder res = new StringBuilder();
+
+        // allocation mémoire pour l'adresse du tableau
+        res.append("\tadd $sp, $sp, -4\n");
+        // allocation mémoire pour la taille du tableau et affectation de la valeur
+        tailleTab.toMIPS();
+        res.append("\tsw $v0, ($sp)\n");
+        res.append("\tadd $sp, $sp, -4\n");
+
+        return res.toString();
+    }
 }
