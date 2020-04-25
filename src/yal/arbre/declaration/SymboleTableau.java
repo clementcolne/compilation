256a0edba1f34abca3d9aebb1c1784eaf4ba9fc9
colne5u <clement.colne@outlook.com>
2020-04-25 17:45:50 +0200

    Update toMIPS symboleTableau

--- src/yal/arbre/declaration/SymboleTableau.java
+++ src/yal/arbre/declaration/SymboleTableau.java
@@ -39,10 +39,10 @@ public class SymboleTableau extends Symbole {
         res.append(tailleTab.toMIPS() + "\n");
         if(Tds.getInstance().identifier(etq, noLig,"tableau",0).getNoBloc() == 0) {
             res.append("\tsw $v0, " + deplacement + "($s7)\n");
-            res.append("\tmove $sp, " + deplacement + "($s7)\n");
+            res.append("\tsw $sp, " + (deplacement - 4) + "($s7)\n");
         }else{
             res.append("\tsw $v0, " + deplacement + "($s2)\n");
-            res.append("\tmove $sp, " + deplacement + "($s2)\n");
+            res.append("\tsw $sp, " + (deplacement - 4) + "($s2)\n");
         }
         res.append("\taddu $sp, $sp, $v0\n\n");
 
